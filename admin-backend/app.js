0771c67308d08841b39270f85ddc35ff62335ea2
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2018-06-25 07:54:05 -0300

    first commit

---
+++ admin-backend/app.js
@@ -0,0 +1,28 @@
+// server.js
+'use strict';
+
+const express = require('express');
+const app = express();
+// Import the required dependencies
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// We are going to implement a JWT middleware that will ensure the validity of our token. We'll require each protected route to have a valid access_token sent in the Authorization header
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://{YOUR-AUTH0-DOMAIN}.auth0.com/.well-known/jwks.json"
+    }),
+    // This is the identifier we set when we created the API
+    audience: '{YOUR-API-AUDIENCE-ATTRIBUTE}',
+    issuer: "{YOUR-AUTH0-DOMAIN}", // e.g., you.auth0.com
+    algorithms: ['RS256']
+});
